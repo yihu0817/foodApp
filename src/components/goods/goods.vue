46bd4ab3206e28b191b52ee265cd3904723c387b
zhousir <2915858910@qq.com>
2019-02-19 22:42:03 +0800

    商品滚动联动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="goods">
     <div class="menu-wrapper" v-el:menu-wrapper>
-        <ul>
-            <li v-for="item in goods" class="menu-item" :key=item>
-                <span class="text border-1px">
-                    <!-- <span v-show="item.type>0" class="icon" :class="classMap[item.type]">{{item.name}}</span> -->
-                    <span >{{item.name}}</span>
-                </span>
-            </li>
-        </ul>
+      <ul>
+        <li
+          v-for="item in goods"
+          class="menu-item"
+          :key="item"
+          :class="{'current':currentIndex === $index}"
+          @click="selectMenu($index,$event)"
+        >
+          <span class="text border-1px">
+            <!-- <span v-show="item.type>0" class="icon" :class="classMap[item.type]">{{item.name}}</span> -->
+            <span>{{item.name}}</span>
+          </span>
+        </li>
+      </ul>
     </div>
     <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list" :key=item>
+        <li v-for="item in goods" class="food-list food-list-hook" :key=item>
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item" :key=food>
-              <div class="icon" >
+              <div class="icon">
                 <img :src="food.icon" width="57px" height="57px">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
-                <div class="extra"> 
-                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}</span>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -50,8 +58,22 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
     };
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i;
+        }
+      }
+      return 0;
+    }
   },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -61,14 +83,41 @@ export default {
         this.goods = response.data;
         this.$nextTick(() => {
           this._initScroll();
+          this._calculateHeight();
         });
       }
     });
   },
   methods: {
+    selectMenu(index, event) {
+      if (!event._constructed) {
+        return;
+      }
+      let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+      let el = foodList[index];
+      this.foodsScroll.scrollToElement(el, 300);
+      console.log('selectNume ' + index);
+    },
     _initScroll() {
-      this.menuScroll = new BScroll(this.$els.menuWrapper, {});
-      this.foodsScroll = new BScroll(this.$els.foodsWrapper, {});
+      this.menuScroll = new BScroll(this.$els.menuWrapper, {
+        click: true
+      });
+      this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+        probeType: 3
+      });
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    _calculateHeight() {
+      let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
     }
   }
 };
@@ -91,7 +140,16 @@ export default {
         display: table
         height:54px
         width: 56px
+        padding: 0 12px
         line-height: 14px
+        &.current
+            position: relative
+            z-index: 10
+            margin-top: -1px
+            background: #fff
+            font-weight: 700
+            .text
+               border-none() 
         .icon
             display: inline-block
             vertical-align: top
@@ -166,10 +224,6 @@ export default {
             text-decoration: line-through
             font-size: 10px
             color: rgb(147, 153, 159)
-
-
-
-
 </style>
 
 
